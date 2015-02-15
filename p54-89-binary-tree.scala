ea0524efc0bd8ab8cdc3a33c8274ff31449050a9
arun <arunrv2011@gmail.com>
2015-02-15 22:34:09 +0530

    p55 - list of all completely balanced binary trees

--- p54-89-binary-tree.scala
+++ p54-89-binary-tree.scala
@@ -1,6 +1,7 @@
 package binaryTree
 
 sealed abstract class Tree[+T]
+
 case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
     override def toString = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
 }
@@ -10,4 +11,26 @@ case object End extends Tree[Nothing] {
 
 object Node {
     def apply[T](value: T):Node[T] = Node(value, End, End)
+}
+
+object Tree {
+    // Problem 55
+    // Construct completely balanced binary trees 
+    def cBalanced[T](n: Int, x: T): List[Tree[T]] = 
+        if (n <= 0) 
+            List(empty[T])
+        else {
+            if ((n - 1) % 2 == 0){
+                val subs = cBalanced((n - 1)/2, x)
+                for{ s1 <- subs 
+                     s2 <- subs } yield(Node(x, s1, s2))
+            }
+            else {
+                val l1 = for{ s1 <- cBalanced((n - 1)/2, x)
+                              s2 <- cBalanced((n - 1)/2 + 1, x) } yield(List(Node(x, s1, s2), Node(x, s2, s1)))
+                l1.foldLeft(List.empty[Tree[T]])((accum, l) => l ::: accum)
+            }
+        }
+
+    def empty[T]:Tree[T] = End
 }
