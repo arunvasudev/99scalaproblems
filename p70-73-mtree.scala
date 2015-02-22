7d1d03e52cb668854393fe0c8495a0ca4c01481a
arun <arunrv2011@gmail.com>
2015-02-22 23:20:37 +0530

    p70c - implemented the nodeCount method

--- p70-73-mtree.scala
+++ p70-73-mtree.scala
@@ -6,6 +6,8 @@ case class MTree[+T](value: T, children: List[MTree[T]]) {
             "M(" + value.toString + " {" + children.map(_.toString).mkString(",") + "})"
         else
             "M(" + value.toString + ")"
+
+    def nodeCount: Int = 1 + children.map(_.nodeCount).sum
 }
 
 object MTree {
