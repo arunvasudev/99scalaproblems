653c04b17491c2bd9412b40627c128e42d0ed164
arun <arunrv2011@gmail.com>
2015-02-24 17:34:13 +0530

    p80 - fromFriendlyString function etc. for Digraph and Graph

--- p80-89-graphs.scala
+++ p80-89-graphs.scala
@@ -2,7 +2,7 @@ package graphs
 
 abstract class GraphBase[T, U]{
     
-    case class Edge(n1: Node, n2: Node, value: U){
+    case class Edge(n1: Node, n2: Node, value: Option[U]){
         def toTuple = (n1.value, n2.value, value)
     }
 
@@ -45,7 +45,7 @@ class Graph[T, U] extends GraphBase[T, U] {
         else if (e.n2 == n) Some(e.n1)
         else None
 
-    def addEdge(n1: T, n2: T, value: U) = {
+    def addEdge(n1: T, n2: T, value: Option[U]) = {
         val node1 = addNode(n1)
         val node2 = addNode(n2)
         val e = new Edge(node1, node2, value)
@@ -56,13 +56,16 @@ class Graph[T, U] extends GraphBase[T, U] {
 
     override def toString: String = {
         import scala.collection.mutable
-        val edgesStr = edges.map(e => e.n1.value.toString + "-" + e.n2.value.toString)
+        val edgesStr = edges.map(e => {
+            e.n1.value.toString + "-" + e.n2.value.toString + 
+            (if (e.value.nonEmpty) {"/" + e.value.get.toString} else "")
+        })
 
         val nodesPrinted = new mutable.HashSet[T]
         edges.foreach(e => {nodesPrinted += e.n1.value; nodesPrinted += e.n2.value} )
 
         val nodesStr = nodes.keys.filter(n => !nodesPrinted.contains(n)).map(_.toString)
-        "[" + (edgesStr ++ nodesStr).mkString(",") + "]"
+        "[" + (edgesStr ++ nodesStr).mkString(", ") + "]"
     }
 }
 
@@ -76,46 +79,77 @@ class Digraph[T, U] extends GraphBase[T, U] {
         if (e.n1 == n) Some(e.n2)
         else None
 
-    def addArc(source: T, dest: T, value: U) = {
+    def addArc(source: T, dest: T, value: Option[U]) = {
         val sourceNode = addNode(source)
         val destNode = addNode(dest)
         val e = new Edge(sourceNode, destNode, value)
         edges = edges + e
         nodes(source).adj = e :: sourceNode.adj
     }
+
+    override def toString: String = {
+        import scala.collection.mutable
+        val edgesStr = edges.map(e => {
+            e.n1.value.toString + ">" + e.n2.value.toString + 
+            (if (e.value.nonEmpty) {"/" + e.value.get.toString} else "")
+        })
+
+        val nodesPrinted = new mutable.HashSet[T]
+        edges.foreach(e => {nodesPrinted += e.n1.value; nodesPrinted += e.n2.value} )
+
+        val nodesStr = nodes.keys.filter(n => !nodesPrinted.contains(n)).map(_.toString)
+        "[" + (edgesStr ++ nodesStr).mkString(", ") + "]"
+    }
 }
 
 object Graph {
-    def term[T, U](nodes: List[T], edges: List[(T, T)], defLabel: U): Graph[T, U] = {
+    def term[T, U](nodes: List[T], edges: List[(T, T)]): Graph[T, U] = {
         val graph = new Graph[T, U]
         nodes.foreach(n => graph.addNode(n))
-        edges.foreach({case (n1, n2) => graph.addEdge(n1, n2, defLabel)})
+        edges.foreach({case (n1, n2) => graph.addEdge(n1, n2, None)})
         graph
     }
 
-    def adjacentList[T, U](list: List[(T, List[T])], defLabel: U): Graph[T, U] = {
+    def termLabel[T, U](nodes: List[T], edges: List[(T, T, U)]): Graph[T, U] = {
+        val graph = new Graph[T, U]
+        nodes.foreach(n => graph.addNode(n))
+        edges.foreach({case (n1, n2, v) => graph.addEdge(n1, n2, Some(v))})
+        graph
+    }
+
+    def adjacentList[T, U](list: List[(T, List[T])]): Graph[T, U] = {
         val graph = new Graph[T, U]
         list.foreach({ case (n, otherEnds) => 
             graph.addNode(n)
-            otherEnds.foreach(n1 => graph.addEdge(n, n1, defLabel))
+            otherEnds.foreach(n1 => graph.addEdge(n, n1, None))
         })
 
         graph
     }
 
-    def fromFriendlyForm(str: String): Graph[String, String] = {
+    def adjacentListLabel[T, U](list: List[(T, List[(T, U)])]): Graph[T, U] = {
+        val graph = new Graph[T, U]
+        list.foreach({ case (n, otherEnds) => 
+            graph.addNode(n)
+            otherEnds.foreach({case (n1, v) => graph.addEdge(n, n1, Some(v))})
+        })
+
+        graph
+    }
+
+    def fromFriendlyString(str: String): Graph[String, String] = {
         import java.util
-        val strtok = new util.StringTokenizer(str, " [],-", true)
+        val strtok = new util.StringTokenizer(str, " [],-/", true)
         
         import scala.collection.mutable
         val buf = new mutable.ListBuffer[String]
         while(strtok.hasMoreTokens()) buf += strtok.nextToken
 
         val toks = buf.filter(s => {s != " " && s != ","}).toList
-        fromFriendlyFormToks(toks)
+        fromFriendlyToks(toks)
     }
 
-    def fromFriendlyFormToks(toks0: List[String]): Graph[String, String] = {
+    def fromFriendlyToks(toks0: List[String]): Graph[String, String] = {
         var currToks = toks0
         if (currToks.head != "[")
             throw new Exception("Friendly forms must begin with a '['")
@@ -128,8 +162,13 @@ object Graph {
                         if (!nextToks.isEmpty) throw new Exception("Illegal extra tokens found after ']'")
                         else return graph 
 
+                    case src::"-"::dest::"/"::value::nextToks => {
+                        graph.addEdge(src, dest, Some(value))
+                        currToks = nextToks
+                    }
+
                     case src::"-"::dest::nextToks => {
-                        graph.addEdge(src, dest, "")
+                        graph.addEdge(src, dest, None)
                         currToks = nextToks
                     }
 
@@ -148,20 +187,68 @@ object Graph {
 }
 
 object Digraph {
-    def term[T, U](nodes: List[T], edges: List[(T, T)], defLabel: U): Digraph[T, U] = {
+    def term[T, U](nodes: List[T], edges: List[(T, T)]): Digraph[T, U] = {
         val graph = new Digraph[T, U]
         nodes.foreach(n => graph.addNode(n))
-        edges.foreach({case (n1, n2) => graph.addArc(n1, n2, defLabel)})
+        edges.foreach({case (n1, n2) => graph.addArc(n1, n2, None)})
         graph
     }
 
-    def adjacentList[T, U](list: List[(T, List[T])], defLabel: U): Digraph[T, U] = {
+    def adjacentList[T, U](list: List[(T, List[T])]): Digraph[T, U] = {
         val graph = new Digraph[T, U]
         list.foreach({ case (n, otherEnds) => 
             graph.addNode(n)
-            otherEnds.foreach(n1 => graph.addArc(n, n1, defLabel))
+            otherEnds.foreach(n1 => graph.addArc(n, n1, None))
         })
 
         graph
+    }
+
+    def fromFriendlyString(str: String): Digraph[String, String] = {
+        import java.util
+        val strtok = new util.StringTokenizer(str, " >/,[]", true)
+
+        import scala.collection.mutable
+        val buf = new mutable.ListBuffer[String]
+        while(strtok.hasMoreTokens()) buf += strtok.nextToken()
+
+        val toks = buf.filter(s => (s != " " && s != ",")).toList
+        fromFriendlyToks(toks)
+    }
+
+    def fromFriendlyToks(toks0: List[String]): Digraph[String, String] = {
+        var currToks = toks0
+        if (currToks.head != "[")
+            throw new Exception("Friendly string must begin with a '['")
+        else {
+            val graph = new Digraph[String, String]
+            currToks = currToks.tail
+            while(!currToks.isEmpty){
+                currToks match {
+                    case "]"::nextToks => 
+                        if (!nextToks.isEmpty) throw new Exception("Illegal extra tokens found after ']'")
+                        else return graph
+
+                    case src::">"::dest::"/"::value::nextToks => {
+                        graph.addArc(src, dest, Some(value))
+                        currToks = nextToks
+                    }
+
+                    case src::">"::dest::nextToks => {
+                        graph.addArc(src, dest, None)
+                        currToks = nextToks
+                    }
+
+                    case str::nextToks => {
+                        graph.addNode(str)
+                        currToks = nextToks
+                    }
+
+                    case Nil => throw new Exception("Ran out of tokens while parsing friendly string")
+                }
+            }
+
+            throw new Exception("Friendly forms must end with a ']' - unterminated friendly form found")
+        }
     }
 }
